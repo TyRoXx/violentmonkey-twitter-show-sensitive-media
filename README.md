524d20b17a076985c18188f91489866f80aff879
TyRoXx <tyroxxxx@gmail.com>
2024-04-06 15:04:43 +0200

    Appears to work

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# violentmonkey-twitter-show-sensitive-media
-script that clicks "Show" on sensitive media for you
+# Why
+
+Twitter has gotten a lot more aggressive with marking media as "sensitive" and there seems to be no way anymore to turn the warning off (as of 2024-04-06).
+You have to click "Show" every single time you want to view a "sensitive" image or video.
+
+# What
+
+A user script for https://github.com/violentmonkey/violentmonkey that clicks "Show" for you.
+
+# How
+
+Import the script, idk.
+
+# Compatibility
+
+I only tested in Firefox 124.
+I guess it also works for Greasemonkey etc, but haven't tested it.
---
+++ violentmonkey-twitter-show-sensitive-media.js
@@ -0,0 +1,66 @@
+// ==UserScript==
+// @name     Twitter Show sensitive media automatically
+// @version  1
+// @grant    none
+// @match    https://twitter.com/*
+// ==/UserScript==
+
+console.log('"Show all" script starting');
+
+function showAllBelowNode(parent) {
+    var spans = parent.getElementsByTagName('span');
+    for (var i = 0; i < spans.length; i++) {
+        var span = spans[i];
+        if (span.textContent !== 'Show') {
+            continue;
+        }
+        var show = span;
+        console.log('Clicking on a "Show" button');
+        show.click();
+    }
+}
+
+function tryToShowNewMediaImmediately() {
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            if (mutation.type !== 'childList') {
+                return;
+            }
+            mutation.addedNodes.forEach(function (node) {
+                if (node.nodeType !== Node.ELEMENT_NODE) {
+                    return;
+                }
+                showAllBelowNode(node);
+            });
+        });
+    });
+
+    // Configure the MutationObserver to observe changes to the entire DOM tree
+    var observerConfig = {
+        // Observe changes to the child nodes (i.e., new elements added)
+        childList: true,
+        // Observe changes to the entire DOM tree
+        subtree: true
+    };
+
+    // Start observing the DOM with the configured options
+    observer.observe(document.body, observerConfig);
+}
+
+function keepShowingAllRegularly() {
+    let oneSecond = 1000;
+    // this should be quick enough for browsing
+    let delay = 0.5 * oneSecond;
+    setTimeout(function () {
+        showAllBelowNode(document);
+        keepShowingAllRegularly();
+    }, delay);
+}
+
+window.addEventListener('load', function () {
+    // install an event handler to Show media as soon as they appear
+    tryToShowNewMediaImmediately();
+    // Sometimes (10-20%) the event handler fails to make the image appear (some race condition?).
+    //
+    keepShowingAllRegularly();
+});
